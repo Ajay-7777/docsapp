29723b14d39ac0f79db31c675351fc17dc458bd3
Ajay Panchal <ajay-panchal@Ajay-Panchal.local>
2024-03-11 20:11:50 +0530

    message

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <footer className="footer" >
+            <div className="footer-container" >
+                <div>
+                  
+                </div>
+                <div >
+                   
+                </div>
+
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
